b2a49315cd13e9af3ec8b734b62ccb0967d83e13
Gustaf Lindqvist <gustaf@gustaf.nu>
2013-12-06 23:44:27 +0100

    Random bg image (closes #4)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('hellbergApp')
   .controller('MainCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.img_index = "0" + Math.floor(Math.round(Math.random() * 9));
     $scope.detail1 = "";
     $scope.detail2 = "";
 
