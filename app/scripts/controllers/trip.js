e32a1f1c2ecb6a6af6e3fd2c86bee991d7bf7a8a
Gustaf Lindqvist <gustaf@gustaf.nu>
2013-12-07 14:42:42 +0100

    Speak

--- app/scripts/controllers/trip.js
+++ app/scripts/controllers/trip.js
@@ -1,19 +1,20 @@
 'use strict';
 
 angular.module('hellbergApp')
-  .controller('TripCtrl', ['$scope', '$routeParams', '$q', '$timeout', 'RouteLoader', 'Questions', function ($scope, $routeParams, $q, $timeout, RouteLoader, Questions) {
+  .controller('TripCtrl', ['$scope', '$routeParams', '$q', '$timeout', 'RouteLoader', 'Questions', 'Speak', function ($scope, $routeParams, $q, $timeout, RouteLoader, Questions, Speak) {
 
     $scope.points = 10;
 
-    $scope.question = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor orci porttitor enim ultricies semper. Nulla turpis nibh, eleifend et sapien sit amet, ullamcorper molestie nulla.";
-
     $scope.loading = true;
 
-    RouteLoader.fetch($routeParams.dep_ref, $routeParams.dest_ref).then(function(res) {
-      Questions.fetch(res[0], res[1], [])
-/*
-      function(questions) {
+    $scope.brake = function() {
+      console.log("BRAKE");
+    }
 
+    RouteLoader.fetch($routeParams.dep_ref, $routeParams.dest_ref).then(function(res) {
+      Questions.fetch(res[0], res[1], []).then(function(questions) {
+
+        var hyperlapse;
         var NUMBER_OF_QUESTIONS = 5;
         var NPTS = 10; // number of points
         var TIME = 50; // total time in seconds
@@ -22,7 +23,56 @@ angular.module('hellbergApp')
         $scope.loader_progress = 0;
         $scope.loader_total = NPTS;
 
-        var hyperlapse = new Hyperlapse(document.getElementById('pano'), {
+        var start = function() {
+          $scope.$apply(function() {
+            $scope.loading = false;
+          });
+
+          var trip_time = 0;
+          var next_question_time = 0;
+          var paused = false, question_idx = 0;
+
+          var pause = function() {
+            paused = true;
+            hyperlapse.pause();
+          };
+
+          var play = function() {
+            hyperlapse.play();
+          }
+
+          $scope.current_score = 2*(NUMBER_OF_QUESTIONS+1);
+          $scope.trip_progress = 0;
+          $scope.question = "";
+
+          $scope.brake = function() {
+            if (paused) {
+              play();
+            } else {
+              pause();
+            }
+          }
+
+          var loop = function() {
+            $scope.trip_progress = (trip_time/1000)/TIME*100;
+
+            if (trip_time >= next_question_time) {
+              next_question_time += TIME*1000/NUMBER_OF_QUESTIONS;
+              $scope.current_score -= 2;
+              var text = questions.getQuestion(question_idx++).question;
+              $scope.question = text;
+              Speak.speak(text);
+            }
+
+            trip_time += 100;
+            if (!paused) {
+              $timeout(loop, 100, true);
+            }
+          };
+          loop();
+        }
+
+        hyperlapse = new Hyperlapse(document.getElementById('pano'), {
           use_lookat: false,
           max_points: NPTS,
           elevation: 50,
@@ -52,34 +102,14 @@ angular.module('hellbergApp')
 
         hyperlapse.onLoadComplete = function(e) {
           hyperlapse.play();
-          $scope.$apply(function() {
-            $scope.loading = false;
-          });
+          start();
         };
 
         hyperlapse.generate({route:res[2]});
 
-        $scope.current_score = 2*NUMBER_OF_QUESTIONS;
-        var trip_time = 0;
-        var next_question_time = 0;
-
-        $scope.current_score = 2*(NUMBER_OF_QUESTIONS+1);
-
-        $scope.trip_progress = 0;
-        var loop = function() {
-          $scope.trip_progress = (trip_time/1000)/TIME*100;
-          $timeout(loop, 100, true);
-          if (trip_time >= next_question_time) {
-            next_question_time += TIME*1000/NUMBER_OF_QUESTIONS;
-            $scope.current_score -= 2;
-          }
-
-          trip_time += 100;
-        };
-        loop();
 
       });
-*/
+
 
     });
 
---
+++ app/scripts/services/speak.js
@@ -0,0 +1,36 @@
+
+
+angular.module('hellbergApp').factory('Speak', ['LOCALE', function(LOCALE) {
+  var instance = {}
+
+
+  instance.speak = function(text) {
+    if (!text) {
+      return false;
+    }
+
+    var audioURL = ['http://www.corsproxy.com/', 'translate.google.com/translate_tts?ie=UTF-8&q=', text , '&tl=', encodeURIComponent(LOCALE.locale)].join('');
+    var audio = new Audio();
+
+    audio.addEventListener("play", function () {
+      console.log('isSpeaking start');
+    }, false);
+
+    audio.addEventListener("ended", function () {
+      justSpoke = true;
+      console.log('isSpeaking end');
+    }, false);
+
+    audio.addEventListener("error", function () {
+      console.log('error');
+      console.log('isSpeaking end');
+    }, false);
+
+    audio.autoplay = true;
+    audio.src = audioURL;
+
+    return audio;
+  };
+
+  return instance;
+}]);
