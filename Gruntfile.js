3dd33e6be69287eb114e9eb42318abd8b2d0e209
Gustaf Lindqvist <gustaf@gustaf.nu>
2013-12-09 08:02:40 +0100

    more cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -226,8 +226,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
-            'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'fonts/*',
             'resources/*'
