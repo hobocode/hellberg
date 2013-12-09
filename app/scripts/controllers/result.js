e741bf2559bc1ee2b2d9bbde9711f6b47dd988dd
Gustaf Lindqvist <gustaf@gustaf.nu>
2013-12-09 12:44:27 +0100

    better loading and play again link

--- app/scripts/controllers/result.js
+++ app/scripts/controllers/result.js
@@ -4,4 +4,8 @@ angular.module('hellbergApp')
   .controller('ResultCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams) {
     $scope.score = $routeParams.score;
     $scope.answer = $routeParams.answer;
+
+    $scope.play = function() {
+      $location.path("/");
+    };
   }]);
